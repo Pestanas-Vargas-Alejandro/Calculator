dce5b22d8360830638564d13a62f10f9c59f0d43
Pestañas-Vargas-Alejandro <isc19350316@gmail.com>
2023-03-02 20:37:23 -0600

    Esquematización de la tabla

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Calculator</h1>
+      <table className="mt-3" style={{width: "400px"}}>
+        <tr>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+        </tr>
+        <tr>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+        </tr>
+        <tr>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+        </tr>
+        <tr>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+        </tr>
+        <tr>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+          <td style={{border: "1px solid black"}}>1</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export default App
