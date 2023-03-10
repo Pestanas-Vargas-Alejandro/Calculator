d9445fc3bd70fa073aca7ba525eb3671e7eceb3d
Pestanas-Vargas-Alejandro <isc19350316@gmail.com>
2023-03-10 10:02:30 -0600

    Hace funcionar el boton 9 mediante una variable de estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useState } from 'react'
 import './App.css'
 
+const buttonsClases = "btn btn-outline-secondary w-100 p-3"
+const buttonClases = "btn btn-outline-secondary w-100 p-5"
+
 function App() {
+  const [screen, setScreen] = useState('0')
+  const handleButtonClick = () => {
+    if (screen === '0') {
+      setScreen(9)
+    } else {
+      setScreen (`${screen}9`)
+    }
+  }
+
   return (
     <div className="container mt-5">
       <h1>Calculator</h1>
@@ -8,7 +21,7 @@ function App() {
         {/*FIrst row*/}
         <tr>
           <td colSpan={4}>
-            <div className='card'> 0
+            <div className='card'> {screen}
               <div className='card-body'>
               </div>
             </div>
@@ -16,36 +29,36 @@ function App() {
         </tr>
         {/*Second row*/}
         <tr>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'> C</button> </td>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>/</button></td>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>*</button></td>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>-</button></td>
+          <td> <button type='button' className={buttonsClases}> C</button> </td>
+          <td> <button type='button' className={buttonsClases}>/</button></td>
+          <td> <button type='button' className={buttonsClases}>*</button></td>
+          <td> <button type='button' className={buttonsClases}>-</button></td>
         </tr>
         {/*Three row*/}
         <tr>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>7</button></td>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>8</button></td>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>9</button></td>
-          <td rowSpan={2}> <button type='button' className='btn btn-outline-secondary w-100 p-5'>+</button></td>
+          <td> <button type='button' className={buttonsClases}>7</button></td>
+          <td> <button type='button' className={buttonsClases}>8</button></td>
+          <td> <button type='button' className={buttonsClases} onClick = {handleButtonClick} >9</button></td>
+          <td rowSpan={2}> <button type='button' className={buttonClases}>+</button></td>
         </tr>
         {/*Four row*/}
         <tr>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>4</button></td>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>5</button></td>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>6</button></td>
+          <td> <button type='button' className={buttonsClases}>4</button></td>
+          <td> <button type='button' className={buttonsClases}>5</button></td>
+          <td> <button type='button' className={buttonsClases}>6</button></td>
         </tr>
         {/*Five row*/}
         <tr>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>1</button></td>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>2</button></td>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>3</button></td>
-          <td rowSpan={2}> <button type='button' className='btn btn-outline-secondary w-100 p-5'>=</button></td>
+          <td> <button type='button' className={buttonsClases}>1</button></td>
+          <td> <button type='button' className={buttonsClases}>2</button></td>
+          <td> <button type='button' className={buttonsClases}>3</button></td>
+          <td rowSpan={2}> <button type='button' className={buttonClases}>=</button></td>
         </tr>
         {/*Six row*/}
         <tr>
-          <td colSpan={1}> <button type='button' className='btn btn-outline-secondary w-100 p-3'>Del</button></td>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>0</button> </td>
-          <td> <button type='button' className='btn btn-outline-secondary w-100 p-3'>.</button> </td>
+          <td colSpan={1}> <button type='button' className={buttonsClases}>Del</button></td>
+          <td> <button type='button' className={buttonsClases}>0</button> </td>
+          <td> <button type='button' className={buttonsClases}>.</button> </td>
         </tr>
       </table>
     </div>
